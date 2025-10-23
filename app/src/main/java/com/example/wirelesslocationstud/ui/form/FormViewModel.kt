1b115837338089ed181efe57786e986c8ab91dde
anaiva <a.ivanchenko.eu@gmail.com>
2025-10-23 16:19:31 +0300

    rename to form

--- app/src/main/java/com/example/wirelesslocationstud/ui/form/FormViewModel.kt
+++ app/src/main/java/com/example/wirelesslocationstud/ui/form/FormViewModel.kt
@@ -1,4 +1,4 @@
-package com.example.wirelesslocationstud.ui.notifications
+package com.example.wirelesslocationstud.ui.form
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class NotificationsViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is notifications Fragment"
+        value = "This is form Fragment"
     }
     val text: LiveData<String> = _text
 }
